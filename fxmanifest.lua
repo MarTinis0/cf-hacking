9715a7fdc87d21b1b0a30baa3825fc54d5c1a71d
MarTinis0 <snkb312@gmail.com>
2024-05-25 00:40:27 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+description 'CodeiFy Scripts, Edit By : @B7Rrr'
+author 'CodeiFy DEV TEAM '
+version '1.0.0'
+lua54 'yes'
+game 'gta5'
+client_script 'client.lua'
+ui_page 'html/index.html'
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/scripts/*.js',
+    'html/images/*',
+    'html/fonts/*',
+    'html/sounds/*',
+}
